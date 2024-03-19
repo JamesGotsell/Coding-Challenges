d18bbdd6554471faac4271f926dbcb6ed7442437
James Gotsell <j.e.gotsell@gmail.com>
2024-03-19 21:25:24 +0000

    done the node WC app

--- node_projects/create_wc/index.ts
+++ node_projects/create_wc/index.ts
@@ -1,1 +1,23 @@
-console.log("test")
+import * as fs from 'fs';
+
+const main = () => {
+    let numOfLines =0 
+const initialValue = 0;
+    let temp:any = []
+    console.log("test")
+    fs.readFileSync(process.argv[2]).toString()
+        .split("\n")
+        .forEach((line) => {
+          
+
+            numOfLines++
+            temp.push(line.length)
+            console.log(line)
+        });
+
+    let wc = temp.reduce((accumulator: number, currentValue: number) => accumulator + currentValue, initialValue)
+    console.log(numOfLines) 
+    console.log(wc)      
+}
+
+main()
